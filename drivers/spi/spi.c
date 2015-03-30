aa85c8ee78c67cc7e17d0da6fb2253ae072f714d
matsi <matsi@example.org>
2015-03-31 00:53:42 +0300

    Minor style changes in spi library
    Signed-off-by: matsi <matsi@example.org>

--- drivers/spi/spi.c
+++ drivers/spi/spi.c
@@ -20,14 +20,9 @@
     WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
     DEALINGS IN THE SOFTWARE.
-
-    $Id$
 */
 
 #include "spi.h"
-
-#include <avr/io.h>
-#include <avr/interrupt.h>
 
 #define PORT_SPI    PORTB
 #define DDR_SPI     DDRB
@@ -36,53 +31,55 @@
 #define DD_SS       DDB2
 #define DD_SCK      DDB5
 
-
-void spi_init()
-// Initialize pins for spi communication
+void spi_init(void)
 {
+
+	/* initialize pins for spi communication */
     DDR_SPI &= ~((1<<DD_MOSI)|(1<<DD_MISO)|(1<<DD_SS)|(1<<DD_SCK));
-    // Define the following pins as output
+
+    /* define the following pins as output */
     DDR_SPI |= ((1<<DD_MOSI)|(1<<DD_SS)|(1<<DD_SCK));
 
 
-    SPCR = ((1<<SPE)|               // SPI Enable
-            (0<<SPIE)|              // SPI Interupt Enable
-            (0<<DORD)|              // Data Order (0:MSB first / 1:LSB first)
-            (1<<MSTR)|              // Master/Slave select
-            (0<<SPR1)|(1<<SPR0)|    // SPI Clock Rate
-            (0<<CPOL)|              // Clock Polarity (0:SCK low / 1:SCK hi when idle)
-            (0<<CPHA));             // Clock Phase (0:leading / 1:trailing edge sampling)
+    SPCR = ((1 << SPE)|					// SPI Enable
+            (0 << SPIE)|				// SPI Interupt Enable
+            (0 << DORD)|				// Data Order (0:MSB first / 1:LSB first)
+            (1 << MSTR)|				// Master/Slave select
+            (0 << SPR1)|(1 << SPR0)|	// SPI Clock Rate
+            (0 << CPOL)|				// Clock Polarity (0:SCK low / 1:SCK hi when idle)
+            (0 << CPHA));				// Clock Phase (0:leading / 1:trailing edge sampling)
 
-    SPSR = (1<<SPI2X);              // Double Clock Rate
-
+	/* double clock rate */
+    SPSR = (1 << SPI2X);
 }
 
+/* shift full array through target device */
 void spi_transfer_sync (uint8_t * dataout, uint8_t * datain, uint8_t len)
-// Shift full array through target device
 {
-       uint8_t i;
-       for (i = 0; i < len; i++) {
-             SPDR = dataout[i];
-             while((SPSR & (1<<SPIF))==0);
-             datain[i] = SPDR;
-       }
+	uint8_t i;
+
+	for (i = 0; i < len; i++) {
+		SPDR = dataout[i];
+		while ((SPSR & (1 << SPIF)) == 0);
+		datain[i] = SPDR;
+	}
 }
 
+/* shift full array to target device without receiving any byte */
 void spi_transmit_sync (uint8_t * dataout, uint8_t len)
-// Shift full array to target device without receiving any byte
 {
-       uint8_t i;
-       for (i = 0; i < len; i++) {
-             SPDR = dataout[i];
-             while((SPSR & (1<<SPIF))==0);
-       }
+	uint8_t i;
+
+	for (i = 0; i < len; i++) {
+		SPDR = dataout[i];
+		while ((SPSR & (1 << SPIF)) == 0);
+	}
 }
 
+/* clocks only one byte to target device and returns the received one */
 uint8_t spi_fast_shift (uint8_t data)
-// Clocks only one byte to target device and returns the received one
 {
-    SPDR = data;
-    while((SPSR & (1<<SPIF))==0);
-    return SPDR;
+	SPDR = data;
+	while ((SPSR & (1 << SPIF)) == 0);
+	return SPDR;
 }
-
--- include/spi.h
+++ include/spi.h
@@ -20,20 +20,17 @@
     WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
     DEALINGS IN THE SOFTWARE.
-
-    $Id$
 */
 
-#ifndef _SPI_H_
-#define _SPI_H_
+#ifndef _AVR_SPI_H_
+#define _AVR_SPI_H_
 
 #include <avr/io.h>
-
+#include <avr/interrupt.h>
 
 extern void spi_init();
 extern void spi_transfer_sync (uint8_t * dataout, uint8_t * datain, uint8_t len);
 extern void spi_transmit_sync (uint8_t * dataout, uint8_t len);
 extern uint8_t spi_fast_shift (uint8_t data);
 
-
-#endif /* _SPI_H_ */
+#endif /* _AVR_SPI_H_ */
