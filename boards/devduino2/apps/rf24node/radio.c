8171f5316948416daceee42e4407e068de45a6f3
matsi <matsi@example.org>
2015-04-09 01:08:28 +0300

    Define and configure all nRF24 pins
    NB: SS pin should be configured as output even if we don't use it. Our AVR MCU is SPI
    master, so we can use any available output pin as CSN signal. However SS should be
    configured as output anyway in order to avoid floating levels and unexpected SPI behavior.
    
    At the moment IRQ pin is configured as input but not yet used.
    
    Signed-off-by: matsi <matsi@example.org>

--- boards/devduino2/apps/rf24node/radio.c
+++ boards/devduino2/apps/rf24node/radio.c
@@ -3,24 +3,29 @@
 
 /* rf24 pins on devduino2 */
 
-#define DD_MOSI     DDB3
-#define DD_MISO     DDB4
-#define DD_SCK      DDB5
+#define DD_CE	DDB0
+#define DD_SS	DDB2
+#define DD_MOSI	DDB3
+#define DD_MISO	DDB4
+#define DD_SCK	DDB5
 
-#define DD_CSN		DDD7
-#define DD_CE       DDB0
+#define DD_IRQ	DDD2
+#define DD_CSN	DDD7
 
 /* */
 
 void spi_init_pins(void)
 {
 	/* reset pins */
-    DDRB &= ~((1<<DD_MOSI)|(1<<DD_MISO)|(1<<DD_SCK)|(1<<DD_CE));
-    DDRD &= ~(1<<DD_CSN);
+    DDRB &= ~((1<<DD_MOSI)|(1<<DD_MISO)|(1<<DD_SCK)|(1<<DD_CE)|(1<<DD_SS));
+    DDRD &= ~((1<<DD_CSN)|(1<<DD_IRQ));
 
     /* define the following pins as output */
     DDRB |= ((1<<DD_MOSI)|(1<<DD_SCK)|(1<<DD_CE));
     DDRD |= (1<<DD_CSN);
+
+    /* NB: we have to set SS as output no matter we do not use it */
+    DDRB |= (1<<DD_SS);
 }
 
 /* */
