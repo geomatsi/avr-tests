84293e46b7d56bed83cafd73782020a4483a6af2
Sergey Matyukevich <geomatsi@gmail.com>
2015-05-07 22:15:45 +0300

    Add support for new message type: sensor data list of variable length
    - add message type with repeated sensor_data field
    - simple implementation of using sensor data message of variable length
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- boards/devduino2/apps/rf24node/main.c
+++ boards/devduino2/apps/rf24node/main.c
@@ -18,6 +18,43 @@
 
 FILE uart_stream = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
 
+/* */
+
+bool sensor_callback(pb_ostream_t *stream, const pb_field_t *field, void * const *arg)
+{
+    sensor_data sensor = {};
+
+    uint32_t data[2];
+    uint32_t idx;
+
+    data[0] = (uint32_t) read_vcc();
+    data[1] = (uint32_t) read_temp_mcp9700();
+
+    for (idx = 0; idx < 2; idx++) {
+
+        printf("protobuf encoding: (%lu, %lu)\n", idx, data[idx]);
+
+        sensor.type = idx;
+        sensor.data = data[idx];
+
+        /* Encode the header for the field, based on the constant info from pb_field_t */
+        if (!pb_encode_tag_for_field(stream, field)) {
+			printf("protobuf tag encoding failed: %s\n", PB_GET_ERROR(stream));
+            return false;
+        }
+
+        /* Encode the data for the field, based on our sensor_data structure */
+        if (!pb_encode_submessage(stream, sensor_data_fields, &sensor)) {
+			printf("protobuf submessage encoding failed: %s\n", PB_GET_ERROR(stream));
+            return false;
+        }
+    };
+
+    return true;
+}
+
+/* */
+
 int main (void)
 {
 	struct rf24 *nrf;
@@ -26,11 +63,10 @@ int main (void)
 	uint8_t buf[20];
 
 	uint8_t rf24_status;
-	long vcc, temp;
 	int ret;
 
+	sensor_data_list message = {};
     pb_ostream_t stream;
-	sensor_data message;
     bool pb_status;
     size_t pb_len;
 
@@ -55,19 +91,13 @@ int main (void)
 
 	while (1){
 
-		vcc = read_vcc();
-		printf("... vcc = %ld\n", vcc);
-
-		temp = read_temp_mcp9700();
-		printf("... temp = %ld\n", temp);
-
 		memset(buf, 0x0, sizeof(buf));
 		stream = pb_ostream_from_buffer(buf, sizeof(buf));
 
-        message.type = 0x1;
-        message.data = (uint32_t) vcc;
+        message.sensor.funcs.encode = &sensor_callback;
+        message.sensor.arg = NULL;
 
-        pb_status = pb_encode(&stream, sensor_data_fields, &message);
+        pb_status = pb_encode(&stream, sensor_data_list_fields, &message);
         pb_len = stream.bytes_written;
 
 		if (!pb_status) {
