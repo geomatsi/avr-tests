2b5ba1f2fd583b7f9e8fcb62a66711a74039ea03
Sergey Matyukevich <geomatsi@gmail.com>
2016-01-10 21:14:50 +0300

    Add new spi declarations
    - new methods: spi_set_mode, spi_deinit
    - spi mode defines: follow Linux spidev.h conventions
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- include/spi.h
+++ include/spi.h
@@ -28,7 +28,22 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+/* follow Linux spidev conventions */
+
+#define SPI_CPHA		0x01
+#define SPI_CPOL		0x02
+
+#define SPI_MODE_0		(0|0)
+#define SPI_MODE_1		(0|SPI_CPHA)
+#define SPI_MODE_2		(SPI_CPOL|0)
+#define SPI_MODE_3		(SPI_CPOL|SPI_CPHA)
+
+/* */
+
 extern void spi_init();
+extern void spi_set_mode(uint8_t mode);
+extern void spi_deinit();
+
 extern void spi_transfer_sync (uint8_t * dataout, uint8_t * datain, uint8_t len);
 extern void spi_transmit_sync (uint8_t * dataout, uint8_t len);
 extern uint8_t spi_fast_shift (uint8_t data);
